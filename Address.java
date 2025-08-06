b480ab7d6a2ee0db297020481160e9c9d247f973
KMateenKhan <120782951+KMateenKhan@users.noreply.github.com>
2025-08-06 10:26:54 +0530

    Address.java

--- Address.java
+++ Address.java
@@ -1,16 +1,23 @@
 class Address
 {
+	// this class is aggregated to the demo file
+	
+	// instance variables
 	int plot;
 	String location;
+
+	// constructor
 	Address(int plot,String location  )
 	{
 		this.plot=plot;
 		this.location=location;
 	}
+	// method
 	public void display()
 	{
 		System.out.println("Plot number:"+plot);
 		System.out.println("Location:"+location);
 	}
+
 
 }
