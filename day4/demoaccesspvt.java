48170303361ec29d039f3dd9f8a73689dfa769b4
KMateenKhan <120782951+KMateenKhan@users.noreply.github.com>
2025-08-07 15:59:34 +0530

    Add files via upload

---
+++ day4/demoaccesspvt.java
@@ -0,0 +1,55 @@
+// accessing private methods outside class using Java_Reflection
+
+import java.lang.reflect.Method;
+ 
+class sampledemo{
+  private String str="welcome";
+ 
+  private void displayPvt()
+  {
+    System.out.println("Accessing this method outside of the class");
+  }
+ 
+ /* public void display()
+  {
+	System.out.println("Accessing the public method"); 
+  }
+ */
+ // it also works for the public methods
+
+  private void prints()
+  {
+	System.out.println("Accessing the second private method.");
+  }
+}
+ 
+class demoaccesspvt
+ {
+      public static void main(String[] args) throws Exception {
+
+        /* sampledemo obj=new sampledemo();
+         obj.displayPvt();  throws error */
+
+        /* Class c=Class.forName("sampledemo");
+	 Object obj=c.newInstance();
+	 Method m1 = c.getDeclaredMethod("displayPvt");
+	 m1.setAccessible(true);
+	 m1.invoke(obj);*/
+
+
+	sampledemo obj=new sampledemo();
+
+	//to search for class "A" and the method "displayPvt"
+	Method m=sampledemo.class.getDeclaredMethod("prints");
+
+	//to bypass java access control checks
+	m.setAccessible(true);
+
+	//calls the private method "diplayPvt" on object obj
+	m.invoke(obj);
+
+	
+
+ 
+      }
+ }
