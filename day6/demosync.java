be2056151a4b13c4a359df5f0bcec2ab80a76de7
KMateenKhan <120782951+KMateenKhan@users.noreply.github.com>
2025-08-11 12:34:31 +0530

    Update demosync.java

--- day6/demosync.java
+++ day6/demosync.java
@@ -1,8 +1,8 @@
 class call
 {
-   synchronized  public void callme(String msg)
+   synchronized  public void callme(String msg) // ensures only one thread access at a time
 	{
-          System.out.print("[");
+      System.out.print("[");
 	  System.out.print(msg);
 	  System.out.print("]");
 	}
@@ -33,10 +33,9 @@ class demosync
     public static void main(String asd[])
 	{
 	   call obj=new call();
-    	   caller c1=new caller(obj,"hello");
+       caller c1=new caller(obj,"hello"); // three caller objects are created
 	   caller c2=new caller(obj,"hi");
-	   caller c3=new caller(obj,"welcome");
- 
-	 
+	   caller c3=new caller(obj,"welcome"); 
    	}
+
   }
