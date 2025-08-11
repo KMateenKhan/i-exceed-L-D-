4ac1ad214c5e9c7bd7962547d79adb4884c95b99
KMateenKhan <120782951+KMateenKhan@users.noreply.github.com>
2025-08-11 11:11:25 +0530

    Update example1.java

--- assessing/example1.java
+++ assessing/example1.java
@@ -6,5 +6,11 @@ class example1.java
       String s2 = new String("java"); // new string object in heap memory
       System.out.println(s1 == s2); // compares the references not the content
       System.out.println(s1.equals(s2)); // compare the content
+
+      System.out.println(s1==s2.intern()); 
+      // The intern() method returns a canonical representation of the string. In simpler terms:
+      //   It checks if a string with the same content already exists in the String pool.
+      //   If it does, it returns the reference from the pool.
+      //   If it doesn’t, it adds the string to the pool and returns that reference.
     }
   }
