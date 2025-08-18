299274fbed0e2a07a5a1e1f9e0501aa835aefbb4
KMateenKhan <120782951+KMateenKhan@users.noreply.github.com>
2025-08-18 10:07:39 +0530

    Add files via upload

---
+++ day9/ArrayListExample.java
@@ -0,0 +1,21 @@
+import java.util.*;
+ 
+class ArrayListExample {
+ 
+   public static void main(String[] args) {
+        // Creating list using the ArrayList class
+        List<String> names= new ArrayList <String> ();
+
+	names.add("adam");
+	names.add("bilal");
+	names.add("danish");
+ 
+        // Access element from the list
+        String name = names.get(3);
+        System.out.println("Accessed Element: " + name);
+ 
+        // Remove element from the list
+        names.remove("bilal");
+        System.out.println("After Removed name: " + names);
+   }
+}
